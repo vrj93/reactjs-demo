fca3944c0cead8a6bbb801e19eb4061e76d57392
Vivek Joshi <vivekjoshi@topsinfosolutions.com>
2022-02-07 17:09:47 +0530

    import, export and some functionality.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {name, age} from "./person.js";
+import message from './message.js';
 
-const myfirstelement = <h1>Hello React!</h1>
+class Car {
+    constructor(name){
+        this.brnad = name;
+    }
 
-ReactDOM.render(myfirstelement, document.getElementById('root'));
+    present(){
+        return "This is my " + this.brnad;
+    }
+}
+
+class Model extends Car {
+    constructor(name, model){
+        super(name);
+        this.model = model;
+    }
+
+    show(){
+        return this.present() + ", It is a " + this.model;
+    }
+}
+
+// const mycar = new Model("Porche", "Turbo");
+// const fruits = ['apple', 'banana', 'orange'];
+
+const details = [name, age];
+const detailslist = details.map((item) => <p>{item}</p>);
+
+ReactDOM.render(message(), document.getElementById('root'));
