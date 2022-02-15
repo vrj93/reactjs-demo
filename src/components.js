1ea562272b499b773ad3cd37ad97a64b4ae23ed8
Vivek Joshi <vivekjoshi@topsinfosolutions.com>
2022-02-15 10:16:27 +0530

    Array map method

--- src/components.js
+++ src/components.js
@@ -6,6 +6,10 @@ function Oddwheel(prop){
     return <h2>This is a {prop.odd.model} wheel drive.</h2>;
 }
 
+function LuxuryCar(prop) { 
+    return <p><b>This is a {prop.luxurymodel}</b></p>;
+}
+
 function TwoWheel(prop){
     const oddwheel = { name: '7.5', model:'6' };
     const shoot = (a) => {
@@ -15,6 +19,11 @@ function TwoWheel(prop){
         alert(k.type);
     }
     const isGoal = prop.isGoal;
+    const models = [
+        {id: 1, brand: 'Ford'},
+        {id: 2, brand: 'BMW'},
+        {id: 3, brand: 'Audi'}
+    ];
 
     //Conditional operator using 'if'
     /*if (isGoal) {
@@ -43,10 +52,18 @@ function TwoWheel(prop){
     return (
         <>
             <h2>This is a 2 wheel drive.</h2>
+            
+            {/* conditional operator */}
             {isGoal.length > 0 &&
                <FourWheel num="4" /> 
             }
             <Oddwheel odd={oddwheel} />
+            
+            {/* array map method */}
+            <ul>
+                {models.map((model) => <LuxuryCar key={model.id} luxurymodel={model.brand}/>)}
+            </ul>
+            
             <button onClick={() => shoot("Button is Clicked!")}>Take the shot!</button>
             <br></br>
             <button onClick={(event) => shoot2("random", event)}>Click!</button>
